cf2c3cb813f3c92e1e47db8803ed12c57831ed07
Lee Chae Rin <chaerin0916@gmail.com>
2024-10-13 12:51:17 +0900

    수정

--- Book/그리디/1이 될 때까지.py
+++ Book/그리디/1이 될 때까지.py
@@ -1,16 +1,13 @@
-# # N, K
-# n, k = map(int, input().split())
-# cnt = 0
-# while(True):
-#     if(n%k==0):
-#         n = n / k
-#     else:
-#         n -= 1
-#     cnt+=1
+# N, K
+n, k = map(int, input().split())
+cnt = 0
+while(True):
+    if(n%k==0):
+        n = n / k
+    else:
+        n -= 1
+    cnt+=1
 
-#     if(n==1):
-#         print(cnt)
-#         break
-
-# 다른 풀이
-# 
+    if(n==1):
+        print(cnt)
+        break
