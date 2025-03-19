055ec8f7c3b34f7b2c41b6f2e5c16d42f0eb29f7
Chaerin Lee <82634482+Rix01@users.noreply.github.com>
2025-03-20 02:14:53 +0900

    [Silver II] Title: 스택 수열, Time: 2672 ms, Memory: 38876 KB -BaekjoonHub

---
+++ 백준/Silver/1874. 스택 수열/스택 수열.py
@@ -0,0 +1,35 @@
+n = int(input())
+inputList = [0]*n
+stackList = [1]
+result = ['+']
+
+for i in range(n):
+    inputList[i] = int(input())
+
+idx = 0
+stackNum = 1
+flag = 0
+# 리스트 길이 0 될 때까지 반복
+while((idx)!=n):
+    now = stackList[-1] # 맨 뒤에. 스택으로 치면 맨 위
+    if(now==inputList[idx]):
+        idx+=1  # 리스트 다음 값 비교
+        stackList.pop() # 값 꺼내기
+        result.append('-')
+        if(len(stackList)==0 and stackNum<n):
+            stackNum+=1
+            result.append('+')
+            stackList.append(stackNum)
+    else:
+        stackNum += 1
+        if(stackNum>n):
+            flag = 1
+            print("NO")
+            break
+        result.append('+')
+        stackList.append(stackNum)
+
+if flag==0:
+    for i in result:
+        print(i, end='\n')
+
