e86d85481d02315750b0972e3064d3c50e0e1ca0
Chaerin Lee <82634482+Rix01@users.noreply.github.com>
2025-03-20 02:19:27 +0900

    Update 스택 수열.py [설명 주석 추가]

--- 백준/Silver/1874. 스택 수열/스택 수열.py
+++ 백준/Silver/1874. 스택 수열/스택 수열.py
@@ -9,13 +9,15 @@ for i in range(n):
 idx = 0
 stackNum = 1
 flag = 0
-# 리스트 길이 0 될 때까지 반복
-while((idx)!=n):
+# 입력값 리스트 인덱스 < n 이면 반복
+while(idx < n):
     now = stackList[-1] # 맨 뒤에. 스택으로 치면 맨 위
     if(now==inputList[idx]):
         idx+=1  # 리스트 다음 값 비교
         stackList.pop() # 값 꺼내기
         result.append('-')
+        # 마지막에 stack 크기가 0이 되었을 때도 +가 되는 경우 발생
+        # 따라서, stackNum 관련 조건도 추가.
         if(len(stackList)==0 and stackNum<n):
             stackNum+=1
             result.append('+')
