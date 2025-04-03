f8a5197be0234ce14846b8224ba202dd8ee61bcd
Chaerin Lee <82634482+Rix01@users.noreply.github.com>
2025-04-03 23:03:14 +0900

    [level 2] Title: 가장 큰 수, Time: 1305.76 ms, Memory: 23.8 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
@@ -1,25 +1,10 @@
 def solution(numbers):
-    numberStr = []
-    
-    for i in numbers:
-        # 숫자를 문자열로 바꾸기
-        numberStr.append(str(i))
-    
-    # 정렬 기준
-    # 1. 맨 앞 자리 기준
-    # 2. 30 3 34 이런 식으로 있을 때 3이 30보다 앞에 있기 위해서는
-        # 자릿수가 작은 경우 33 이런 식으로 되게 해야 함. => 자릿수 맞춰줘야 한다는 소리.
-        # 문제 제한 조건 보면 원소는 1000이하임. 
-        # 따라서 3자리 수까지만 맞춰주면 됨.
-        # 999 990 900 9 => 999 9 990 900 순으로 정렬될 것임.
-        # 즉, 각 숫자마다 *3 해주면 한 자리수가 3자리가 되니까 비교 가능해짐.
-    numberStr = sorted(numberStr, key=lambda x:x*3, reverse=True)
+    # 문자열로 바꿔야지
+    for i in range(len(numbers)):
+        numbers[i] = str(numbers[i])
+    answer = sorted(numbers, key=lambda x:x*3, reverse=True)
+    res = ''.join(answer)
 
-    # 정렬 순서대로 join
-    # answer = ''.join(numberStr)로 하면 0,0,0과 같은 경우 000으로 반환됨.
-    # 따라서 int로 감쌌다가 str로 다시 형변환 해야 함
-    answer = str(int(''.join(numberStr)))
-    return answer
-
-numbers = [34, 31, 35, 3, 30]
-print(solution(numbers))
+    # 단, 000000 같은 경우가 생길 수 있으므로 한번 숫자로 변환 후에 다시 str로 바꾸기!
+    res = str(int(res))
+    return res;
