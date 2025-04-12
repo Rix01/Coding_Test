07cb20f512c79ef6e8a8745a5123d9367eb21023
Chaerin Lee <82634482+Rix01@users.noreply.github.com>
2025-04-12 15:43:32 +0900

    [level 2] Title: 주식가격, Time: 112.72 ms, Memory: 18.5 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.py
+++ 프로그래머스/2/42584. 주식가격/주식가격.py
@@ -1,11 +1,9 @@
 def solution(prices):
     answer = []
     for i in range(len(prices)-1):
-        time = 0
         compareValue = prices[i]
         for j in range(i+1, len(prices), 1):
             if(compareValue>prices[j]):
-                time = j-i
                 break
         answer.append(j-i)
     # 마지막은 무조건 0
