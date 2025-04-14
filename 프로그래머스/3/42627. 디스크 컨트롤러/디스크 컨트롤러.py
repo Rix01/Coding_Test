99cd87251b4b45d4c8898cc149685f4a4e1040ce
Chaerin Lee <82634482+Rix01@users.noreply.github.com>
2025-04-15 07:15:53 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 9.26 ms, Memory: 9.29 MB -BaekjoonHub

---
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.py
@@ -0,0 +1,24 @@
+from heapq import heappop, heappush
+
+def solution(jobs):
+    answer = 0
+    now = 0 # 현재 시간
+    complete = 0 # 완료한 프로세스
+    preTime = -1 # 전 프로세스의 요청 시간
+    waitingHeap = []
+
+    while(complete<len(jobs)):
+        for j in jobs:
+            if(preTime<j[0]<=now):
+                heappush(waitingHeap, [j[1], j[0]])
+        
+        if(len(waitingHeap)>0):
+            job = heappop(waitingHeap)
+            preTime = now
+            now += job[0]
+            complete+=1
+            answer += now - job[1]
+
+        else:
+            now+=1
+    return answer//len(jobs)
