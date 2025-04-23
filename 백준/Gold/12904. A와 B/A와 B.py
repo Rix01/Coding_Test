c7078906f8671fbe90f1f2e1aafefd179eddea24
Chaerin Lee <82634482+Rix01@users.noreply.github.com>
2025-04-24 02:30:34 +0900

    [Gold V] Title: A와 B, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Gold/12904. A와 B/A와 B.py
@@ -0,0 +1,18 @@
+import sys
+input = sys.stdin.readline
+
+s = list(input().strip())
+t = list(input().strip())
+
+# t에서 거꾸로 s가 가능한지 여부 판단
+for i in range(len(t)-1, len(s)-1, -1):
+    if t[-1] == 'A':
+        t.pop()
+    elif t[-1] == 'B':
+        t.pop()
+        t.reverse()
+if s == t:
+    print(1)
+
+else:
+    print(0)
