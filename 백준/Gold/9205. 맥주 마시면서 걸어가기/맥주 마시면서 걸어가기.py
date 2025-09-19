3f4f0a39fc98ec5003d6640289acfcb9aaf9de01
Chaerin Lee <82634482+Rix01@users.noreply.github.com>
2025-09-19 22:19:41 +0900

    [Gold V] Title: 맥주 마시면서 걸어가기, Time: 112 ms, Memory: 34952 KB -BaekjoonHub

--- 백준/Gold/9205. 맥주 마시면서 걸어가기/맥주 마시면서 걸어가기.py
+++ 백준/Gold/9205. 맥주 마시면서 걸어가기/맥주 마시면서 걸어가기.py
@@ -1,40 +1,34 @@
 from collections import deque
 
-# 맨해튼 거리 구하는 함수
 def distance(p1, p2):
-    return abs(p2[0] - p1[0]) + abs(p2[1] - p1[1])
+    return abs(p1[0] - p2[0]) + abs(p1[1] - p2[1])
 
 def solution():
     n = int(input())
     positions = []
-    for _ in range(n + 2):
+    for _ in range(n+2):
         x, y = map(int, input().split())
         positions.append((x, y))
+    # 방문여부
+    visited = [0] * (n+2)
 
     start = positions[0]
     end = positions[-1]
-
-    # 최대 이동 가능 거리
-    max_dist = 1000
-
     queue = deque([start])
-    visited = [0] * (n+2)
     visited[0] = 1
+
+    max_distance = 1000 # 어차피 중간중간 갈 수 있는 곳 있으면 리필 되니까 최대 1000m 이내에 있는지 여부만 확인하면 됨
 
     while queue:
         now = queue.popleft()
 
-        # 도착지 도달 여부 확인
         if now == end:
             return "happy"
 
-        # 다음 위치 이동 가능 여부 확인
-        for i in range(n+2):
-            next = positions[i]
-            if not visited[i] and distance(now, next) <= max_dist:
+        for i in range(len(positions)):
+            if(not visited[i] and distance(positions[i], now) <= max_distance):
                 visited[i] = 1
-                queue.append(next)
-    # 다 돌고도 도착 못함
+                queue.append(positions[i])
     return "sad"
 
 t = int(input())
