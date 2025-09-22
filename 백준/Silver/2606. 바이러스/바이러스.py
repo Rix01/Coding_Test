f764f1638224973d8516f6eb4fc2c1de57b1baf1
Chaerin Lee <82634482+Rix01@users.noreply.github.com>
2025-09-22 19:52:08 +0900

    [Silver III] Title: 바이러스, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.py
+++ 백준/Silver/2606. 바이러스/바이러스.py
@@ -1,46 +1,24 @@
-# 이것도 BFS 문제인 것 같은디?
-from collections import deque
+# 41분에 시작
 
-queue = deque()
+n = int(input())
+line = int(input())
 
-comNum = int(input())
-connectNum = int(input())
-connections = dict()
+computer = [[0]*(n+1) for _ in range(n+1)]
+visited = [0]*(n+1)
+count = 0
 
-visited = [0]*(comNum+1) # 0번 없으니까
+for _ in range(line):
+    n1, n2 = map(int, input().split())
+    computer[n1][n2] = 1
+    computer[n2][n1] = 1
 
-# 연결 정보 만들기
-# {1: [2, 5], 2: [1, 3, 5], 3: [2], 5: [1, 2, 6], 6: [5], 4: [7], 7: [4]}
-for i in range(connectNum):
-    com1, com2 = map(int, input().split())
-    if (com1 not in connections):
-        connections[com1] = [com2]
-    elif (com1 in connections):
-        connections[com1].append(com2)
+def dfs(node):
+    global count
+    visited[node] = 1
+    count += 1
+    for i in range(1, n+1):
+        if(visited[i] == 0 and computer[node][i] == 1):
+            dfs(i)
 
-    if (com2 not in connections):
-        connections[com2] = [com1]
-    elif (com2 in connections):
-        connections[com2].append(com1)
-# print(connections)
-
-# 1이랑 연결된 게 아무것도 없는 경우
-if(1 not in connections):
-    cnt = 0
-else:
-    queue.append(1)    # 1부터 시작
-    visited[1] = 1
-    cnt = 0
-    while(len(queue)!=0):
-        now = queue.popleft()
-
-        # now의 원소 개수만큼 for문 반복
-        for v in connections[now]:
-            if visited[v] == 1: # 이미 방문했다면 pass
-                continue
-            else:
-                cnt+=1
-                visited[v] = 1
-                queue.append(v)
-
-print(cnt)
+dfs(1)
+print(count-1)
