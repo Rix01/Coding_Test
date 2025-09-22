0a5f16a8c17fba17494d91af89c3fd9573d85de0
Chaerin Lee <82634482+Rix01@users.noreply.github.com>
2025-09-22 20:26:53 +0900

    [Silver I] Title: 단지번호붙이기, Time: 36 ms, Memory: 32488 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
@@ -1,42 +1,42 @@
-# 출력값은 단지 수, 각 단지별 집 수
-import sys
-sys.setrecursionlimit(10**6)
-input = sys.stdin.readline
+# 53분 시작
 
 n = int(input())
-town = []
+apartments = []
+flag = 0
+count = []
+
+dx = [1, -1, 0, 0]
+dy = [0, 0, 1, -1]
+
+for _ in range(n):
+    row = list(map(int, input()))
+    apartments.append(row)
+
 visited = [[0]*n for _ in range(n)]
-for i in range(n):
-    town.append(list(map(int, input().strip())))
 
-# 상하좌우
-dx = [-1, 1, 0, 0]
-dy = [0, 0, -1, 1]
+def dfs(now):
+    global flag
+    global count
+    visited[now[0]][now[1]] = flag
+    count[flag-1] += 1
 
-homeCnt = 0
-def dfs(a, b):
-    global homeCnt
-    if town[a][b] == 1 and visited[a][b] == 0:
-        visited[a][b] = 1
-        homeCnt += 1
+    for i in range(4):
+        nx, ny = now[0] + dx[i], now[1] + dy[i]
 
-        for i in range(4):
-            nx, ny = a+dx[i], b+dy[i]
-            if 0 <= nx < n and 0 <= ny < n:
-                dfs(nx, ny)
+        if(0 <= nx < n and 0 <= ny < n and apartments[nx][ny] == 1 and visited[nx][ny] == 0):
+            dfs((nx, ny))
 
-townCnt = 0
-homeCnts = []
-for i in range(n):
-    for j in range(n):
-        if town[i][j] == 1 and visited[i][j] == 0:
-            townCnt += 1
-            homeCnt = 0
-            dfs(i, j)
-            homeCnts.append(homeCnt)
+def solution():
+    global flag
 
-print(townCnt)
-# 오름차순 정렬.
-homeCnts.sort()
-for h in homeCnts:
-    print(h)
+    for i in range(n):
+        for j in range(n):
+            if(visited[i][j] == 0 and apartments[i][j] == 1):
+                flag += 1
+                count.append(0)
+                dfs((i,j))
+
+solution()
+print(flag)
+count.sort()
+print(*count, sep="\n")
