5a78a54e8838bd630a7d7d6c30f32f5d9177e4b7
Chaerin Lee <82634482+Rix01@users.noreply.github.com>
2025-10-01 22:17:19 +0900

    [level 2] Title: 주식가격, Time: 70.97 ms, Memory: 18.6 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.py
+++ 프로그래머스/2/42584. 주식가격/주식가격.py
@@ -1,11 +1,18 @@
+from collections import deque
+
 def solution(prices):
     answer = []
-    for i in range(len(prices)-1):
-        compareValue = prices[i]
-        for j in range(i+1, len(prices), 1):
-            if(compareValue>prices[j]):
+    queue = deque(prices)
+
+    while queue:
+        current = queue.popleft()
+        cnt = 0
+        for next_price in queue:
+            if current <= next_price:
+                cnt += 1
+            else:
+                cnt += 1
                 break
-        answer.append(j-i)
-    # 마지막은 무조건 0
-    answer.append(0)
+        answer.append(cnt)
+
     return answer
