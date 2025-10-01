2ce3a2788bfefd18e0ffa3625d24ff3b30a55e19
Chaerin Lee <82634482+Rix01@users.noreply.github.com>
2025-10-02 00:13:47 +0900

    [level 3] Title: 베스트앨범, Time: 0.11 ms, Memory: 9.21 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
@@ -1,48 +1,27 @@
-def solution(genres, plays):
-    answer = []
-    # [[0, 500], [1, 600], [2, 150], [3, 800], [4, 2500]]
-    index_play = list()
-    for i in range(len(plays)):
-        index_play.append([i, plays[i]])
+def solution(genre_array, play_array):
+    result = []
+    genre_index = dict()
+    genre_total = dict()
 
-    # 딕셔너리 형태 만들기
-    # {"classic":[[0, 500], [2, 150], [3, 800]], "plays": [[1, 600], [4, 2500]]} 형태.
-    dict_genres = {}
-    for i in range(len(genres)):
-        if(genres[i] in dict_genres):
-            dict_genres[genres[i]].append(index_play[i])
+    for i in range(len(genre_array)):
+        if genre_array[i] not in genre_index:
+            genre_index[genre_array[i]] = [(i, play_array[i])]
+            genre_total[genre_array[i]] = play_array[i]
         else:
-            dict_genres[genres[i]] = [index_play[i]]
+            genre_index[genre_array[i]].append((i, play_array[i]))
+            genre_total[genre_array[i]] += play_array[i]
 
-    # 딕셔너리 값 1번째 인덱스 기준으로 정렬해서 update
-    # {'classic': [[3, 800], [0, 500], [2, 150]], 'pop': [[4, 2500], [1, 600]]}
-    for k, v in dict_genres.items():
-        dict_genres[k] = sorted(v, key=lambda x:x[1], reverse=True)    
+    # 1. 많이 재생된 장르 먼저 수록 (장르 선택)
+    first_sort = sorted(genre_total, key=lambda x:genre_total[x], reverse=True)
 
-    # 총합 딕셔너리 생성
-    # {'classic': 1450, 'pop': 3100}
-    dict_genres_sum = {}
-    for k, v in dict_genres.items():
-        playSum =0
-        for p in v:
-            playSum += p[1]
-        dict_genres_sum[k] = playSum
+    # 2. 장르 내에서 많이 재생된 노래를 먼저 수록, 재생 횟수가 같은 노래 중에서는 고유 번호가 낮은 노래를 먼저 수록
+    for k, v in genre_index.items():
+        # 정렬 후 다시 넣기
+        genre_index[k] = sorted(v, key=lambda x: (-x[1], x[0]))
 
-    # 총합 추가
-    # {'classic': [[3, 800], [0, 500], [2, 150], 1450], 'pop': [[4, 2500], [1, 600], 3100]}
-    for k, v in dict_genres_sum.items():
-        dict_genres[k].append(v)
+    # 장르별로 돌면서 추가
+    for genre in first_sort:
+        for i in range(min(2, len(genre_index[genre]))):
+            result.append(genre_index[genre][i][0])
 
-    # 딕셔너리 정렬 총합 기준으로.
-    # 
-    dict_genres = dict(sorted(dict_genres.items(), key=lambda x:x[1], reverse=True))
-
-    for k, v in dict_genres.items():
-        if(len(v)==1):
-            answer.append(v[0][0])
-            continue
-        else:
-            answer.append(v[0][0])
-            answer.append(v[1][0])
-
-    return answer
+    return result
