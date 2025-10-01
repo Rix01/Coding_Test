d7558d649a2735aa6d466aeed5ad181ced1dce09
Chaerin Lee <82634482+Rix01@users.noreply.github.com>
2025-10-02 00:15:09 +0900

    [level 2] Title: 올바른 괄호, Time: 10.22 ms, Memory: 9.4 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
@@ -1,14 +1,12 @@
-def solution(s):
+def solution(string):
     stack = []
-    for i in s:
-        if(i=="("):
-            stack.append(i)
-        elif(i==")"):
-            # stack이 0이라면
-            if(len(stack)==0):
+    for s in string:
+        if(s == "("):
+            stack.append(s)
+        elif(s == ")"):
+            if(len(stack) == 0):
                 return False
             stack.pop()
     if(len(stack)==0):
         return True
-    else:
-        return False
+    return False
