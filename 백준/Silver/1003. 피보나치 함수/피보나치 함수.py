d43244e3cbc33b24ef2e318f005fcc3d1cbaaad7
Chaerin Lee <82634482+Rix01@users.noreply.github.com>
2025-10-15 22:45:33 +0900

    [Silver III] Title: 피보나치 함수, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1003. 피보나치 함수/피보나치 함수.py
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.py
@@ -1,20 +1,10 @@
 t = int(input())
-fibo = [[0 for i in range(2)] for j in range(41)]
 
-def hap(arr, a, b):
-    result = [0, 0]
-    result[0] = arr[a][0] + arr[b][0]
-    result[1] = arr[a][1] + arr[b][1]
-    return result
-
-fibo[0] = [1, 0]
-fibo[1] = [0, 1]
+dp = [[1,0], [0,1]]
 
 for i in range(2, 41):
-    fibo[i] = hap(fibo, i-1, i-2)
+    dp.append([dp[i-1][0] + dp[i-2][0], dp[i-1][1] + dp[i-2][1]])
 
-for i in range(t):
-    num = int(input())
-    for j in fibo[num]:
-        print(j, end =' ')
-    print()
+for _ in range(t):
+    test_case = int(input())
+    print(dp[test_case][0], dp[test_case][1])
