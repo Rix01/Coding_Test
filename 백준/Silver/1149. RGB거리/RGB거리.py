3b14a98ebd3b13251e1e4941bb1689282a939284
Chaerin Lee <82634482+Rix01@users.noreply.github.com>
2025-10-21 11:16:30 +0900

    [Silver I] Title: RGB거리, Time: 56 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.py
+++ 백준/Silver/1149. RGB거리/RGB거리.py
@@ -1,17 +1,16 @@
-n= int(input())
-rgbs = [[0, 0, 0] for i in range(n)]
+n = int(input())
 cost = []
-
-for i in range(n):
+for _ in range(n):
     cost.append(list(map(int, input().split())))
 
-rgbs[0][0], rgbs[0][1], rgbs[0][2] = cost[0][0], cost[0][1], cost[0][2]
+dp = [[0, 0, 0] for _ in range(n)]
+dp[0][0], dp[0][1], dp[0][2] = cost[0][0], cost[0][1], cost[0][2]
 
 for i in range(1, n):
-    rgbs[i][0] = min(rgbs[i-1][1]+cost[i][0], rgbs[i-1][2]+cost[i][0])
-    rgbs[i][1] = min(rgbs[i-1][0]+cost[i][1], rgbs[i-1][2]+cost[i][1])
-    rgbs[i][2]= min(rgbs[i-1][0]+cost[i][2], rgbs[i-1][1]+cost[i][2])
+    dp[i][0] = min(dp[i-1][1] + cost[i][0], dp[i-1][2] + cost[i][0])
 
-result = min(rgbs[n-1][0], rgbs[n-1][1], rgbs[n-1][2])
+    dp[i][1] = min(dp[i-1][0] + cost[i][1], dp[i-1][2] + cost[i][1])
 
-print(result)
+    dp[i][2] = min(dp[i-1][0] + cost[i][2], dp[i-1][1] + cost[i][2])
+
+print(min(dp[n-1]))
