95a3ba962c99c77e6453e4035a6d1401d485383d
Chaerin Lee <82634482+Rix01@users.noreply.github.com>
2025-10-29 15:51:11 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 572 ms, Memory: 50112 KB -BaekjoonHub

---
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
@@ -0,0 +1,87 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+  static int v, e;
+  static int[] parent;
+  static List<Edge> edges;
+
+  static class Edge implements Comparable<Edge> {
+
+    int start, end, weight;
+
+    Edge(int start, int end, int weight) {
+      this.start = start;
+      this.end = end;
+      this.weight = weight;
+    }
+
+    @Override
+    public int compareTo(Edge o) {
+      return this.weight - o.weight;
+    }
+  }
+
+  static int find(int x) {
+    if (parent[x] == x) {
+      return x;
+    }
+    return parent[x] = find(parent[x]);
+  }
+
+  static void union(int x, int y) {
+    x = find(x);
+    y = find(y);
+    if (x != y) {
+      parent[y] = x;
+    }
+  }
+
+  static int kruskal() {
+    // 간선 기준 정렬
+    Collections.sort(edges);
+
+    int totalCost = 0;
+    int connectCount = 0;
+
+    // 돌면서 수행
+    for(Edge edge: edges) {
+      if(find(edge.start) != find(edge.end)) {
+        union(edge.start, edge.end);
+        totalCost +=  edge.weight;
+        connectCount++;
+
+        if(connectCount == v - 1) break;
+      }
+    }
+    return totalCost;
+  }
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st = new StringTokenizer(br.readLine());
+
+    v = Integer.parseInt(st.nextToken());
+    e = Integer.parseInt(st.nextToken());
+
+    edges = new ArrayList<>();
+    parent = new int[v+1];
+
+    // 부모 초기화
+    for (int i = 1; i <= v; i++) {
+      parent[i] = i;
+    }
+
+    // 간선 입력받기
+    for (int i = 1; i <= e; i++) {
+      st = new StringTokenizer(br.readLine());
+      int start = Integer.parseInt(st.nextToken());
+      int end = Integer.parseInt(st.nextToken());
+      int weight = Integer.parseInt(st.nextToken());
+
+      edges.add(new Edge(start, end, weight));
+    }
+    System.out.println(kruskal());
+  }
+}
