d8c75176f5501a5efef5ea89dad0de9290d91822
Chaerin Lee <82634482+Rix01@users.noreply.github.com>
2025-11-07 13:38:45 +0900

    [Gold III] Title: 소수의 연속합, Time: 192 ms, Memory: 27132 KB -BaekjoonHub

--- 백준/Gold/1644. 소수의 연속합/소수의 연속합.java
+++ 백준/Gold/1644. 소수의 연속합/소수의 연속합.java
@@ -1,69 +1,62 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main {
+  static boolean[] isPrime = new boolean[4_000_001];
+  static List<Integer> primes = new ArrayList<>();
+
   public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-    boolean[] isPrime = new boolean[4_000_001];
-    List<Integer> primes = new ArrayList<>();
+    // n 입력 받기
+    int n = Integer.parseInt(br.readLine());
 
-    // isPrime 배열 초기화
-    for (int i = 2; i <= 4_000_000; i++) {
+    // n까지의 정수까지의 Prime Number 배열 구하기
+    primes = getPrimes(n);
+
+    // 투 포인터로 찾기
+    int start = 0, end = 0, sum = 0, count = 0;
+
+    while(end < primes.size()) {
+      if(sum < n) {
+        sum += primes.get(end++);
+      } else {
+        if(sum==n) count++;
+        sum -= primes.get(start++);
+      }
+    }
+
+    while(sum>=n) {
+      if(sum==n) count++;
+      sum -= primes.get(start++);
+    }
+
+    bw.write(String.valueOf(count) + "\n");
+    bw.close();
+  }
+
+  static List<Integer> getPrimes(int n) {
+    // isPrime true로 초기화
+    for(int i = 2; i <=n ; i++) {
       isPrime[i] = true;
     }
 
-    // 에라토스테네스의 체
-    for (int i = 2; i * i <= 4_000_000; i++) {
-      if (isPrime[i]) {
-        for (int j = i * i; j <= 4_000_000; j += i) {
+    // 에라토스테네스 체 이용해서 소수 구하기
+    for(int i = 2; i*i <=n; i++) {
+      if(isPrime[i]) {
+        for(int j = i*i; j <=n; j+=i ) {
           isPrime[j] = false;
         }
       }
     }
 
-    // 소수 리스트에 추가
-    for (int i = 2; i <= 4_000_000; i++) {
-      if (isPrime[i]) {
+    // isPrime[i] == true인 거 소수 리스트에 넣기
+    for(int i = 2; i<=n; i++) {
+      if(isPrime[i]) {
         primes.add(i);
       }
     }
-
-    // n입력 받기
-    int n = Integer.parseInt(br.readLine());
-
-    // 투 포인터 이용해서 가능한지 찾기
-    int start = 0, end = 0;
-    long hap = 0;
-    int count = 0;
-
-    // end 포인터가 리스트 끝에 도달할 때까지 반복
-    while (end < primes.size()) {
-      if (hap < n) {
-        // 합이 작으면 end를 늘려서 값을 더함
-        hap += primes.get(end);
-        end++;
-      } else {
-        // 합이 같거나 크면 start를 늘려서 값을 뺌
-        if (hap == n) {
-          count++;
-        }
-        hap -= primes.get(start);
-        start++;
-      }
-    }
-    
-    // end에 도달했어도 hap이 n보다 크다면 start를 옮겨서 찾을 수 있음
-    while (hap >= n) {
-      if (hap == n) {
-        count++;
-      }
-      hap -= primes.get(start);
-      start++;
-    }
-
-    bw.write(String.valueOf(count) + "\n");
-    bw.close();
+    return primes;
   }
 }
